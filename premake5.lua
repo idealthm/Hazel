6afe6b94d45e70bf67af988096dd4fdf81a982ac
idealthm <1240568620@qq.com>
2021-10-05 14:28:52 +0800

    after premake

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Hazel"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel"
+	location "Hazel"
+	kind "SharedLib"
+	language "c++"
+
+	targetdir ("bin/" ..outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines 
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL"
+			--"_WINDLL"
+		}
+
+		postbuildcommands
+		{
+			{"{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox"}
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_DELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		symbols "On"
+
+	--filter {"system:windows","configurations:Release"}
+		
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "c++"
+
+	targetdir ("bin/" ..outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src"
+	}
+
+	links
+	{
+		"Hazel"
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines 
+		{
+			"HZ_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_DELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		symbols "On"
